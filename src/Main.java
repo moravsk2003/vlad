0cd84af54e8e5a898f3674860c5cc4aef1b824d3
moravsk2003 <kfcrfdjghjcbvj@gmail.com>
2023-03-01 21:35:54 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //1 завдання
+        int f1=11;
+        int g=2;
+        System.out.println(f1%g);
+        System.out.println((double)f1/g);
+        //2 завдання
+        int f=21;
+        int s=f/10+f%10;
+        System.out.println(s);
+        //3 завдання
+        double k=1.49;
+        int k1=(int)((k%1)*2);
+        int k2=(int)(k1+k);
+        System.out.println(k2);
     }
 }
